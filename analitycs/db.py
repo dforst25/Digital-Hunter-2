512de86e67dce504770d57ccf39138ae019acdf4
David Forst <dforst@g.jct.ac.il>
2026-03-17 17:27:55 +0200

    finished the query 4 successfully

--- analitycs/db.py
+++ analitycs/db.py
@@ -1,7 +1,6 @@
 import os
 
 import mysql.connector
-
 from pprint import pprint
 
 
@@ -63,27 +62,39 @@ class DbConnection:
     # query 3
     def get_top_3_unknown_entities(self):
         query = """
-                SELECT entity_id, SUM(count) as count
-                FROM   (SELECT entity_id, COUNT(*) AS count
-                        FROM intel_signals
-                        WHERE entity_id not in (SELECT entity_id
-                                                FROM targets)
-                        GROUP BY entity_id
-                        UNION ALL
-                        SELECT entity_id, COUNT(*) AS count
-                        FROM attacks
-                        WHERE entity_id not in (SELECT entity_id
-                                                FROM targets)
-                        GROUP BY entity_id
-                        UNION ALL
-                        SELECT entity_id, COUNT(*) AS count
-                        FROM damage_assessments
-                        WHERE entity_id not in (SELECT entity_id
-                                                FROM targets)
-                        GROUP BY entity_id) AS t
+                SELECT entity_id, COUNT(*) AS count
+                FROM intel_signals
+                WHERE priority_level = 99
                 GROUP BY entity_id
                 ORDER BY count DESC
                 LIMIT 3;
+                """
+        return self.run_query(query)
+
+    # query 4
+    def get_extreme_entities(self):
+        query = """
+                SELECT DISTINCT entity_id
+                FROM intel_signals 
+                WHERE TIME(`timestamp`) BETWEEN '08:00:00' AND '20:00:00'
+                GROUP BY entity_id, DATE(`timestamp`)
+                HAVING SUM(distance_from_last) = 0
+                AND entity_id IN (
+                    SELECT entity_id
+                    FROM intel_signals
+                    WHERE TIME(`timestamp`) <= '08:00:00' OR TIME(`timestamp`) <= '08:00:00'
+                    GROUP BY entity_id
+                    HAVING SUM(distance_from_last) >= 10
+                )
+                """
+        return self.run_query(query)
+
+    def get_cords_order_by_date(self, entity_id):
+        query = f"""
+                SELECT reported_lat, reported_lon
+                FROM intel_signals
+                WHERE entity_id = "{entity_id}"
+                ORDER BY timestamp
                 """
         return self.run_query(query)
 
